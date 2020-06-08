18cf5654c7fd2fa1bbee2919bd10ee56bc9963dd
Miah Sanchez <miahsanchez4@gmail.com>
2020-06-08 21:45:13 +0000

    added personalized map styling

--- portfolio/src/main/webapp/aboutme_script.js
+++ portfolio/src/main/webapp/aboutme_script.js
@@ -14,7 +14,169 @@
 
 /** Creates a map and adds it to the page. */
 function createMap() {
-  const map = new google.maps.Map(
+
+    const map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 40.821520, lng: -73.955770}, zoom: 16});
+      {center: {lat: 40.821520, lng: -73.955770}, zoom: 16,
+      styles: 
+        [
+        {
+            "elementType": "geometry",
+            "stylers": [
+            {
+                "color": "#f5f5f5"
+            }
+            ]
+        },
+        {
+            "elementType": "labels.icon",
+            "stylers": [
+            {
+                "visibility": "off"
+            }
+            ]
+        },
+        {
+            "elementType": "labels.text.fill",
+            "stylers": [
+            {
+                "color": "#616161"
+            }
+            ]
+        },
+        {
+            "elementType": "labels.text.stroke",
+            "stylers": [
+            {
+                "color": "#f5f5f5"
+            }
+            ]
+        },
+        {
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels.text.fill",
+            "stylers": [
+            {
+                "color": "#bdbdbd"
+            }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [
+            {
+                "color": "#eeeeee"
+            }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [
+            {
+                "color": "#757575"
+            }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [
+            {
+                "color": "#e5e5e5"
+            }
+            ]
+        },
+        {
+            "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [
+            {
+                "color": "#9e9e9e"
+            }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+            {
+                "color": "#ffffff"
+            }
+            ]
+        },
+        {
+            "featureType": "road.arterial",
+            "elementType": "labels.text.fill",
+            "stylers": [
+            {
+                "color": "#757575"
+            }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [
+            {
+                "color": "#dadada"
+            }
+            ]
+        },
+        {
+            "featureType": "road.highway",
+            "elementType": "labels.text.fill",
+            "stylers": [
+            {
+                "color": "#616161"
+            }
+            ]
+        },
+        {
+            "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [
+            {
+                "color": "#9e9e9e"
+            }
+            ]
+        },
+        {
+            "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [
+            {
+                "color": "#e5e5e5"
+            }
+            ]
+        },
+        {
+            "featureType": "transit.station",
+            "elementType": "geometry",
+            "stylers": [
+            {
+                "color": "#eeeeee"
+            }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "geometry",
+            "stylers": [
+            {
+                "color": "#c9c9c9"
+            }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [
+            {
+                "color": "#9e9e9e"
+            }
+            ]
+         }
+    ]});
 }
