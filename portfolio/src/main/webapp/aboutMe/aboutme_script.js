d86b1e4977a92986b2c57c23426104e13cc1a28e
Miah Sanchez <miahsanchez4@gmail.com>
2020-06-10 19:23:13 +0000

    change href destination

---
+++ portfolio/src/main/webapp/aboutMe/aboutme_script.js
@@ -0,0 +1,251 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+let map;
+const homeCoordinates = {
+    lat: 40.821520, 
+    lng: -73.955770
+}
+
+/** Creates a map and adds it to the page. */
+function createMap() {
+    map = new google.maps.Map(
+      document.getElementById('map'),{
+          center: homeCoordinates, zoom: 16,
+          styles: 
+            [
+            {
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#f5f5f5"
+                }
+                ]
+            },
+            {
+                "elementType": "labels.icon",
+                "stylers": [
+                {
+                    "visibility": "off"
+                }
+                ]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#616161"
+                }
+                ]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                {
+                    "color": "#f5f5f5"
+                }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#bdbdbd"
+                }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#eeeeee"
+                }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#757575"
+                }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#e5e5e5"
+                }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#9e9e9e"
+                }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#ffffff"
+                }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#757575"
+                }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#dadada"
+                }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#616161"
+                }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#9e9e9e"
+                }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#e5e5e5"
+                }
+                ]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#eeeeee"
+                }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                {
+                    "color": "#c9c9c9"
+                }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                {
+                    "color": "#9e9e9e"
+                }
+            ]
+            }
+    ]});
+    makeMarkers();
+}
+
+/** Changes the center of the map. */
+function changeCenter(lat, lng) {
+    map.setCenter({lat, lng});
+}
+
+/** Creates all the markers that will be added to the map. */
+function makeMarkers() {
+    const homeMarker = new google.maps.Marker({
+        position: homeCoordinates, 
+        map: map, 
+        icon: "/images/houseIcon.png"
+    });
+    const infoWindow = new google.maps.InfoWindow({
+        content:"The apartment I grew up in!"
+    });
+    infoWindow.open(map, homeMarker);
+
+    homeMarker.addListener('click', function() {
+          addMarkerImage();
+        });
+
+    const cornellMarker = new google.maps.Marker({
+        position: {
+            lat: 42.444759, 
+            lng: -76.484306
+        },
+        map: map,
+        icon: "/images/cornellIcon.png"
+    });
+
+    const bronxMarker = new google.maps.Marker({
+        position: {
+            lat: 40.825770,
+            lng: -73.852760
+        },
+        map: map,
+        icon: "/images/heartIcon.png"
+    });
+
+    const gwcMarker = new google.maps.Marker({
+        position: {
+            lat: 40.756989,
+            lng: -73.989708
+        },
+        map: map,
+        icon: "images/gwcIcon.png"
+    });
+
+    const drMarker = new google.maps.Marker({
+        position: {
+            lat: 18.483402,
+            lng: -69.929611
+        },
+        map: map,
+        icon: "/images/drIcon.png"
+    });
+}
+
+function addMarkerImage() {
+    let image = document.getElementById('marker-image');
+    image.setAttribute("src", "/images/hamilton_heights.jpg")
+}
