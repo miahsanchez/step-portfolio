3e2364cac2b639b98005a51a1f4f0cd4f3c8d6c8
Miah Sanchez <miahsanchez4@gmail.com>
2020-06-17 14:44:52 +0000

    cleaned up code

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -23,9 +23,7 @@ import java.util.HashSet;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    // this function takes in a collection of events and a meeting request, we are going to 
-    // parse through the meeting
-    request.getAttendees(); // this is going to be the people required to go to the meeting
+    request.getAttendees();
     Collection<String> meetingMembers = request.getAttendees();
     Set<TimeRange> times = new HashSet<>();
     if (request.getDuration() < 24*60){
@@ -35,33 +33,34 @@ public final class FindMeetingQuery {
         Set<TimeRange> modifier = new HashSet<>();
         modifier.addAll(times); // i could also do something where I order the events from start to end and then just add the gaps in between them
         if (request.getAttendees().containsAll(e.getAttendees())){
-        for(TimeRange t: times){
-            if (t.contains(e.getWhen())){
-                modifier.remove(t);
-                TimeRange beforeContainer = TimeRange.fromStartEnd(t.start(), e.getWhen().start(), false);
-                TimeRange afterContainer = TimeRange.fromStartEnd(e.getWhen().end(), t.end(), false);
-                if (beforeContainer.duration() >= request.getDuration()) {
-                    modifier.add(beforeContainer);
+            for(TimeRange t: times){
+                if (t.contains(e.getWhen())){
+                    modifier.remove(t);
+                    TimeRange beforeContainer = TimeRange.fromStartEnd(t.start(), e.getWhen().start(), false);
+                    TimeRange afterContainer = TimeRange.fromStartEnd(e.getWhen().end(), t.end(), false);
+                    if (beforeContainer.duration() >= request.getDuration()) {
+                        modifier.add(beforeContainer);
+                    }
+                    if (afterContainer.duration() >= request.getDuration()) {
+                        modifier.add(afterContainer);
+                    }
                 }
-                if (afterContainer.duration() >= request.getDuration()) {
-                    modifier.add(afterContainer);
+                else if (t.overlaps(e.getWhen())) {
+                    modifier.remove(t);
+                    TimeRange remOverlap;
+                    if (t.start() < e.getWhen().start()) {
+                        remOverlap = TimeRange.fromStartEnd(t.start(), e.getWhen().start(), false);
+                    }else {
+                        remOverlap = TimeRange.fromStartEnd(e.getWhen().end(), t.end(), false);
+                    }
+                    if (remOverlap.duration() >= request.getDuration()){
+                    modifier.add(remOverlap);}
                 }
             }
-            else if (t.overlaps(e.getWhen())) {
-                modifier.remove(t);
-                TimeRange remOverlap;
-                if (t.start() < e.getWhen().start()) {
-                    remOverlap = TimeRange.fromStartEnd(t.start(), e.getWhen().start(), false);
-                }else {
-                    remOverlap = TimeRange.fromStartEnd(e.getWhen().end(), t.end(), false);
-                }
-                if (remOverlap.duration() >= request.getDuration()){
-                modifier.add(remOverlap);}
-            }
-        }
-        times.clear();
-        times.addAll(modifier);
-    }}
+            times.clear();
+            times.addAll(modifier);
+        }   
+    }
     ArrayList<TimeRange> timesList = new ArrayList<>(times);
     Collections.sort(timesList, TimeRange.ORDER_BY_START);
     return timesList;
